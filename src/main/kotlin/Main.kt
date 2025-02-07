2e5737b413b75d2f6828b21bc97a1e24b9d4d582
ArtemDobricki <fpm.dobricki@gmail.com>
2025-02-07 03:26:05 +0300

    feature-1:format things

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,20 @@ package org.example
 import org.example.particles.*
 
 fun main() {
-    val particles = listOf(
-        Particle(10.0, 20.0, 0.0, -1.0, 7.0, '*'),
-        Particle(30.0, 20.0, 0.0, 1.0, 7.0, '0'),
-        Particle(20.0, 10.0, 1.0, 0.0, 7.0, '#'),
-        Particle(20.0, 30.0, -1.0, 0.0, 7.0, '@')
-    )
+  val particles = listOf(
+    Particle(10.0, 20.0, 0.0, -1.0, 7.0, '*'),
+    Particle(30.0, 20.0, 0.0, 1.0, 7.0, '0'),
+    Particle(20.0, 10.0, 1.0, 0.0, 7.0, '#'),
+    Particle(20.0, 30.0, -1.0, 0.0, 7.0, '@')
+  )
 
-    while (true) {
-        clearConsole()
-        val forces= calculateParticlesGravity(particles)
-        updateParticlesPosition(particles, forces)
-        drawParticles(particles, 60)
-        Thread.sleep(1)
-    }
+  while (true) {
+    clearConsole()
+    val forces= calculateParticlesGravity(particles)
+    updateParticlesPosition(particles, forces)
+    drawParticles(particles, 60)
+    Thread.sleep(1)
+  }
 
 
 
